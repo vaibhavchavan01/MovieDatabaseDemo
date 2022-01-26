ae62f2eedbe52e714e242cd4a51a88a4e5cc0b53
vaibhavchavan01 <vaibhavchavan495@gmail.com>
2022-01-26 14:09:18 +0530

    Movie database project commited

---
+++ djangoDemoProject/demoProject/demoApp/backends.py
@@ -0,0 +1,23 @@
+# Django
+from django.contrib.auth.backends import ModelBackend
+# Models
+from .models import User
+from django.db.models import Q
+
+class CustomBackend(ModelBackend):
+    """
+    authentication class to login with the email address.
+    """
+    def authenticate(self, request, email=None, password=None, **kwargs):
+        print("in custome aunthenticate : ", email, password)
+        try:
+            user = User.objects.get(Q(email=email) | Q(mobile=email))
+        except User.DoesNotExist:
+            return None
+
+        if password is None:
+            return None
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
+
+
